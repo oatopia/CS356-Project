2068e43684b7cd23ab0acc84156ebc2023e6a760
oat <oat15050@gmail.com>
2021-06-16 09:28:34 +0700

    edit html file

---
+++ service/AlertLogin.php
@@ -0,0 +1,4 @@
+<?php
+    echo "<script> alert('กรุณาเข้าสู่ระบบ'); </script>";
+    echo "<script> location.href='../html/Login.html'; </script>";
+?>
--- service/Resetpassword.php
+++ service/Resetpassword.php
@@ -9,15 +9,24 @@ if ($conn->connect_error) {
 //Define Value
 $user = $_POST['username'];
 $pass = $_POST['password'];
+$conpass = $_POST['confirmpassword'];
 
 
-mysqli_set_charset($conn, "utf8");
+    mysqli_set_charset($conn, "utf8");
 //Check Email is exist
-    $sql = "UPDATE  customer SET Password='$pass' WHERE Username = '$user'";
-    if ($conn->query($sql) === TRUE) {
-        session_write_close();
+
+    if($pass === $conpass){
+        $sql = "UPDATE  customer SET Password='$pass' WHERE Username = '$user'";
+        if ($conn->query($sql) === TRUE) {
+            session_write_close();
+            $conn->close();
+            echo "<script> alert('บันทึกรหัสผ่านใหม่สำเร็จ'); </script>";
+            echo "<script> location.href='../html/Login.html'; </script>";
+        }
+    }else{
         $conn->close();
-        echo "<script> alert('บันทึกรหัสผ่านใหม่สำเร็จ'); </script>";
-        echo "<script> location.href='../html/Login.html'; </script>";
+        echo "<script> alert('รหัสผ่านไม่ตรงกัน'); </script>";
+        echo "<script> window.history.back(); </script>";
     }
+    
 ?>
